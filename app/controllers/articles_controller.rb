ba88a735ac59c25c9379e02d8b02f100644bdb63
RachelKolk <rach.m.obrien@gmail.com>
2019-07-31 14:56:29 -0400

    edit coded with flash notcies and redirect

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def index
 
   end
@@ -14,6 +18,16 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)       
     else
       render :new
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Artcile was successfully updated."
+      redirect_to article_path(@article)
+    else
+      render :edit
     end
   end
 
