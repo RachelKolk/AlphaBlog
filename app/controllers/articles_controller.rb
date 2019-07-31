51b15ec4e1a70c8a71a0cd44c3b21077d872a0e3
RachelKolk <rach.m.obrien@gmail.com>
2019-07-31 15:15:43 -0400

    articles list view and layout links added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,14 @@
 class ArticlesController < ApplicationController
+  def index
+      @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
 
   def edit
     @article = Article.find(params[:id])
-  end
-
-  def index
-
   end
 
   def create
